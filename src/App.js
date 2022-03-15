b6477d28300decc3337fe7c2ef3ce7b9122e63c0
Kastriot Bislimi <k.bislimi96@gmail.com>
2022-03-15 18:22:04 +0100

    Project

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import './App.css';
+
+import {
+  Blog,
+  Features,
+  Footer,
+  Header,
+  Possibility,
+  WhatGPT3,
+} from "./containers";
+import { CTA, Brand, Navbar, Feature } from "./components";
+
+const App = () => {
+  return <div className="App">
+    <div className="gradient__bg">
+      <Navbar />
+      <Header />
+    </div>  
+    <Brand />
+    <WhatGPT3 />
+    <Feature />
+    <Possibility />
+    <CTA />
+    <Blog />
+    <Footer />
+  </div>;
+};
+
+export default App;
