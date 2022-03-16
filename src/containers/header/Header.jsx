466c0a22f1672aa15945f8cc27ce10fe52b9a990
Kastriot Bislimi <k.bislimi96@gmail.com>
2022-03-16 23:18:09 +0100

    Header Done

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./header.css";
+import people from "../../assets/people.png";
+import ai from "../../assets/ai.png";
 
 function Header() {
   return (
     <div className="gpt3__header section__padding" id="home">
-      <div className="gpt3__header-container">
+      <div className="gpt3__header-content">
         <h1 className="gradient__text">
           Let's Build Something amazing with GPT-3 OpenAI
         </h1>
@@ -17,6 +19,13 @@ function Header() {
           <input type="email" placeholder="Your Email address" />
           <button type="button">Get Started</button>
         </div>
+        <div className="gpt3__header-content__people">
+          <img src={people} alt="people" />
+          <p>1600 people requsted access a visit last 24 hours</p>
+        </div>
+      </div>
+      <div className="gpt3__header-image">
+        <img src={ai} alt="ai" />
       </div>
     </div>
   );
